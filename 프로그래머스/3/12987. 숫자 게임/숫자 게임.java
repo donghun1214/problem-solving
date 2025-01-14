9a81582ab06bc88ce840baf535ef4bb75d527c44
HUNI <110475840+donghun1214@users.noreply.github.com>
2025-01-14 20:17:04 +0900

    [level 3] Title: 숫자 게임, Time: 124.63 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,26 +1,47 @@
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 class Solution {
-    public int solution(int[] A, int[] B) {
-        Arrays.sort(A);
-        Arrays.sort(B);
-
-        int answer = 0;
-        int aIdx = 0;  // A팀 인덱스
-        int bIdx = 0;  // B팀 인덱스
-
-        while (aIdx < A.length && bIdx < B.length) {
-            if (B[bIdx] > A[aIdx]) {
-                // B팀이 승리하면 점수 추가
-                answer++;
-                aIdx++;
-                bIdx++;
+    public static int[] g_A, g_B;
+    public static boolean[] b_used = new boolean[100002];
+    public static int answer = 0;
+    
+    public static int upper_bound(int target){
+        int low = 0;
+        int high = g_B.length;
+        
+        while(low < high){
+            int mid = (low + high) / 2;
+            if (g_B[mid] > target) {
+                high = mid;  
             } else {
-                // B팀이 지면 더 큰 숫자를 찾기 위해 B팀 인덱스만 증가
-                bIdx++;
+                low = mid + 1;  
             }
         }
-
+        return low;
+    }
+    
+    
+    public int solution(int[] A, int[] B) {
+        g_A = A; g_B = B;
+        Arrays.sort(g_A); Arrays.sort(g_B);
+        
+        for(int i : g_A){
+            int idx = upper_bound(i);
+            if(idx >= g_B.length) return answer; //범위에 벗어난 값
+            else{
+                //이전 idx 와 동일한 값 이면 계속 갱신하기
+                while(b_used[idx]){
+                    idx++;
+                }
+                if(idx >= g_B.length) return answer; //범위에 벗어난 값인지 체크
+                answer++; //값 추가해
+                b_used[idx] = true;
+            }
+        }
+        
+    
+        
         return answer;
     }
 }
